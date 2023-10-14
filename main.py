a24f7b9b1aa7330d5ac16207ded4689ebe4dc8df
sofiyaga <sofiyaga@icloud.com>
2023-10-14 10:07:48 +0200

    Add function filter_gc_content

--- main.py
+++ main.py
@@ -4,4 +4,27 @@ def compute_gc_content(seq: str) -> float:
     :param seq: nucleotide sequence
     :return: float, result of computation
     """
-    return ((seq.count('G') + seq.count('C'))/len(seq)) * 100
+    return ((seq.count('G') + seq.count('C')) / len(seq)) * 100
+
+
+def filter_gc_content(seqs: dict, gc_bounds=None) -> dict:
+    """
+    Filters fastq dictionary by GC-content.
+    :param seqs: dict, fastq dictionary.
+    :param gc_bounds: int or tuple
+    :return: dict. Filtered fastq dictionary. If gc_bounds is not defined, returns input dictionary.
+    """
+    if gc_bounds is None:
+        return seqs
+    seqs_filtered: dict = dict()
+    for name, seq in seqs.items():
+        if type(gc_bounds) == tuple:
+            if gc_bounds[0] <= compute_gc_content(seq[0]) <= gc_bounds[1]:
+                seqs_filtered[name] = seq
+        else:
+            if compute_gc_content(seq[0]) <= gc_bounds:
+                seqs_filtered[name] = seq
+    if not seqs_filtered:
+        raise ValueError("Too strict conditions")
+    else:
+        return seqs_filtered
