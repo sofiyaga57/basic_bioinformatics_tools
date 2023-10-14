e6e5e496f84b1e7384a5b06d49955652433e9495
sofiyaga <sofiyaga@icloud.com>
2023-10-14 18:53:14 +0200

    Import main functions

--- basic_tools.py
+++ basic_tools.py
@@ -3,9 +3,9 @@ This script imports three modules and their main functions.
 """
 
 from basic_bioinformatics_tools.dna_rna_tools import run_dna_rna_tools
-# from basic_bioinformatics_tools.protein_tools import run_protein_tools as run_protein_tools
-# from basic_bioinformatics_tools.fastq_tools import run_fastq_tools as run_fastq_tools
+from basic_bioinformatics_tools.protein_tools import run_protein_tools
+from basic_bioinformatics_tools.fastq_tools import run_fastq_tools
 
-print(run_dna_rna_tools('atg', 'reverse'))
-# run_protein_tools()
-# run_fastq_tools()
+run_dna_rna_tools()
+run_protein_tools()
+run_fastq_tools()
