dec8f26bba042fb3e9647de926213bd8100bc981
sofiyaga <sofiyaga@icloud.com>
2023-10-23 00:24:25 +0200

    Add convert_multiline_fasta_to_oneline function

--- basic_bioinformatics_tools/bio_files_processor.py
+++ basic_bioinformatics_tools/bio_files_processor.py
@@ -21,3 +21,33 @@ def read_fasta_file(input_fasta: str) -> dict:
                 seq.append(line.strip())
         fasta_data[current_name] = ''.join(seq)
     return fasta_data
+
+
+def write_fasta_file(fasta_data: dict, output_fasta: str):
+    """"
+    Writes fasta dictionary into fasta file.
+    :param fasta_data: dict, fasta dictionary
+    :param output_fasta: str, name or path of output fasta file.
+    Returns fasta file with written fasta dictionary
+    """
+    with open(output_fasta, mode='w') as file:
+        for seq_id, seq in fasta_data.items():
+            file.write('>' + seq_id + '\n')
+            file.write(seq + '\n')
+
+
+def convert_multiline_fasta_to_oneline(input_fasta: str, output_fasta=None):
+    """
+    Converts multiline fasta file to oneline fasta file
+    :param input_fasta: str, name of input fasta file with .fasta
+    :param output_fasta: str, name of output fasta file without .fasta
+    Returns file
+    """
+    input_fasta = os.path.abspath(input_fasta)
+    if output_fasta is None:
+        output_fasta = os.path.splitext(os.path.basename(input_fasta))[0]
+    output_path = os.path.join(output_fasta + '.fasta')
+
+    fasta_data = read_fasta_file(input_fasta)
+    write_fasta_file(fasta_data=fasta_data, output_fasta=output_path)
+
